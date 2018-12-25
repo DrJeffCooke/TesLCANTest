856fdd4de592e8f70abd40f680c669414d145fb4
Jeff Cooke <24613307+DrJeffCooke@users.noreply.github.com>
2018-12-25 14:53:26 -0600

    Version of the CAN reader w/ Interrupts
    The Battery communicates at 1000KBPS on CAN and thus if you're writing (Serial.println) to the serial port there is a loss of frames.  The MCP2515_CAN only has a 2 frame buffer, so if new frames arrive and the prior frame hasn't been processed (captured by the microcontroller) then the prior frame is overwritten and lost. The core problem is that data is coming in from the BMS faster than the microcontroller can output the frames to the serial port.
    
    This test routine "CANTestToolInterrupt" uses two techniques to get around the problem, 1) interrupts and 2) circular queue.  The MCP2515 interrupt line is connected to pin 2 on the Mega, when the line goes LOW it triggers the interrupt data capture routine.  This routine just stuffs the captured frame into an array (it makes no attempt to output it to serial).  The circular queue has two pointers into it, the addPoint and the readPoint.  The interrupt writes the frames to the addPoint and the loop() reads frames for outputting to Serial from the readPoint.  The reader avoids reading if the addPoint and the readPoint are the same value.  The pointers cycle off the end of the array back to the start (thus circular).  Since the data is being added to the circular queue faster than it is being consumed, there are occasions where a frame will get overwritten by a new frame before the reader gets to it.  This is okay in the case of the BMS because the BMS just continuously blasts out the same frames repeatedly.  So if missed on one pass, the data can be caught on the next cycle.  This would be more problematic is messages were sent once and acted as commands, rather than informative (but that can also be handled at a higher level by sending a command (repeatedly) until a confirmation is received.

---
+++ CANTestToolInterrupt/CANTestToolInterrupt.ino
@@ -0,0 +1,118 @@
+/*
+  TesLorean Battery Control Test
+  2018
+  Jeff Cooke
+
+  Addresses the dropped CAN packets with the MCP2515_CAN interface
+  Uses interrupts to capture the data and outputs and other low priority tasks in the loop()
+  
+*/
+
+#include <mcp2515.h>
+#include <SPI.h>
+
+// Relay Connections - Connector X357 (data connection)
+#define Relay_X357_4  7  // X357-3 to 12v??? accessory wake up
+#define Relay_X357_5  8  // X357-14 to 5v HV energy mgt communication enable
+
+// CAN board Chip Select (CS) pins
+#define BATTERY_CAN_CS 10
+#define TESLOREAN_CAN_CS 9
+
+// DEFINE
+#define StoredFrames 100
+
+// Array for preprepared CAN frames
+can_frame CANFrames[StoredFrames];
+
+// Counters tracking the add point and read point in the circular array
+volatile uint8_t addPointFrames = 0;
+volatile uint8_t readPointFrames = 0;
+volatile uint16_t netAddReadCount = 0;   // Adds increment, Reads decrement, only Read if <> 0
+
+// Create the CAN objects, specific the 'Chip Select' line
+MCP2515 can0(BATTERY_CAN_CS);    // Battery CAN Bus - CS on line 10
+MCP2515 can1(TESLOREAN_CAN_CS);   // TesLorean CAN bus - CS on line 9
+
+struct can_frame frame;
+	
+void irqBATHandler()
+{
+  uint8_t irq = can0.getInterrupts();
+
+  // check channel 0
+  if (irq & MCP2515::CANINTF_RX0IF)
+  {
+    if (can0.readMessage(MCP2515::RXB0, &CANFrames[addPointFrames]) == MCP2515::ERROR_OK)
+    {
+        // frame contains received from RXB0 message
+        addPointFrames = (addPointFrames + 1) % StoredFrames;
+        netAddReadCount++;
+    }
+  }
+            
+  // check channel 1
+  if (irq & MCP2515::CANINTF_RX1IF)
+  {
+    if (can0.readMessage(MCP2515::RXB1, &CANFrames[addPointFrames]) == MCP2515::ERROR_OK)
+    {
+      // frame contains received from RXB1 message
+      addPointFrames = (addPointFrames + 1) % StoredFrames;
+      netAddReadCount++;
+    }
+  } 
+}
+	
+void setup()
+{
+  Serial.begin(115200);
+  SPI.begin();
+
+  // Initialize the array pointers
+  addPointFrames = 0;
+  readPointFrames = 0;
+  netAddReadCount = 0;
+
+  can0.reset();
+  can0.setBitrate(CAN_1000KBPS);
+  can0.setNormalMode();
+
+  //// SETUP OUTPUT LINES - BMS Data Communications
+  pinMode(Relay_X357_4, OUTPUT);
+  pinMode(Relay_X357_5, OUTPUT);
+  digitalWrite(Relay_X357_4, HIGH); // X357-3 to 12v accessory wake up
+  digitalWrite(Relay_X357_5, HIGH); // X357-14 to 5v HV energy mgt communication enable
+  
+  attachInterrupt(digitalPinToInterrupt(2), irqBATHandler, LOW);
+
+  Serial.println("Starting...");
+}
+
+// Send the frame to the Serial port
+void outputCANframe(can_frame & frame)
+{
+  // Send frame to the Serial Port
+  Serial.print("CAN frame : 0x");
+  Serial.print(frame.can_id,HEX);
+  Serial.print(" - ");
+  for (int i=0;i<frame.can_dlc;i++)
+  {
+    Serial.print(" 0x");
+    Serial.print(frame.data[i],HEX);
+  }
+  Serial.println();  
+}
+
+void loop()
+{
+  // Output a captured frame
+  if (netAddReadCount > 0)
+  {
+    if (readPointFrames != addPointFrames)
+    {
+      outputCANframe(CANFrames[readPointFrames]);
+      readPointFrames = (readPointFrames + 1) % StoredFrames;
+      if(netAddReadCount > 0){netAddReadCount--;}
+    }
+  }
+}
